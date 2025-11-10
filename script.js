6e91e5b7fab8e63d900756a9464f29743e41f857
m4rreParre <marrebjoredal@gmail.com>
2025-11-10 19:30:16 +0100

    dsadsa

---
+++ script.js
@@ -0,0 +1,7 @@
+const r1 = document.querySelector("#ett");
+const r2 = document.querySelector("#två");
+const r3 = document.querySelector("#tre");
+
+r1.addEventListener(`change`, () => {});
+r2.addEventListener(`change`, () => {});
+r3.addEventListener(`change`, () => {});
